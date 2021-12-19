7dbf939f695bd8520cbdae82bd304af53935ca08
Mustaque Ahmed <amustaque97@gmail.com>
2021-12-19 17:58:49 +0530

    Sol dive - part two

--- dive/sol.py
+++ dive/sol.py
@@ -1,14 +1,15 @@
 with open('input.txt', encoding='utf-8') as f:
-    x, y = 0, 0
+    x, y, aim = 0, 0, 0
     for instruction in f.readlines():
         direction, unit = instruction.split()
         unit = int(unit)
         if direction == 'forward':
             x += unit
+            y += (aim * unit)
         elif direction == 'down':
-            y += unit
+            aim += unit
         elif direction == 'up':
-            y -= unit
+            aim -= unit
         else:
             pass
 
